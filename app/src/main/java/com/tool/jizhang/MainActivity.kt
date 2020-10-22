1bba39adb5e064d3009987806b71e8e98479c8aa
xx <904118851@qq.com>
2020-10-22 16:00:56 +0800

    123

--- app/src/main/java/com/tool/jizhang/MainActivity.kt
+++ app/src/main/java/com/tool/jizhang/MainActivity.kt
@@ -19,20 +19,35 @@ class MainActivity : BaseActivity() {
     }
 
     fun initData(){
-        val component: TabLayout.Tab = mTabLayout.newTab()
-        component.text = "组件"
-        component.setIcon(if (SettingSPUtils.getInstance().isUseCustomTheme()) R.drawable.custom_selector_icon_tabbar_component else R.drawable.selector_icon_tabbar_component)
-        mTabLayout.addTab(component)
+        val mingxi: TabLayout.Tab = mTabLayout.newTab()
+        mingxi.text = "明细"
+        mingxi.setIcon(R.drawable.img_home_tab_mingxi_g)
+        mTabLayout.addTab(mingxi)
 
-        val util: TabLayout.Tab = mTabLayout.newTab()
-        util.text = "工具"
-        util.setIcon(if (SettingSPUtils.getInstance().isUseCustomTheme()) R.drawable.custom_selector_icon_tabbar_util else R.drawable.selector_icon_tabbar_util)
-        mTabLayout.addTab(util)
+        val count: TabLayout.Tab = mTabLayout.newTab()
+        count.text = "统计"
+        count.setIcon(R.drawable.img_home_tab_count_g)
+        mTabLayout.addTab(count)
 
-        val expand: TabLayout.Tab = mTabLayout.newTab()
-        expand.text = "拓展"
-        expand.setIcon(if (SettingSPUtils.getInstance().isUseCustomTheme()) R.drawable.custom_selector_icon_tabbar_expand else R.drawable.selector_icon_tabbar_expand)
-        mTabLayout.addTab(expand)
+        val kongbai: TabLayout.Tab = mTabLayout.newTab()
+        kongbai.text = ""
+        kongbai.setIcon(R.drawable.img_home_kongbai)
+        mTabLayout.addTab(kongbai)
+
+        val kongbai1: TabLayout.Tab = mTabLayout.newTab()
+        kongbai1.text = ""
+        kongbai1.setIcon(R.drawable.img_home_kongbai)
+        mTabLayout.addTab(kongbai1)
+
+        val reward: TabLayout.Tab = mTabLayout.newTab()
+        reward.text = "福利"
+        reward.setIcon(R.drawable.img_home_tab_reward_g)
+        mTabLayout.addTab(reward)
+
+        val my: TabLayout.Tab = mTabLayout.newTab()
+        my.text = "我的"
+        my.setIcon(R.drawable.img_home_tab_my_g)
+        mTabLayout.addTab(my)
 
         WidgetUtils.setTabLayoutTextFont(mTabLayout)
 
@@ -40,17 +55,44 @@ class MainActivity : BaseActivity() {
 
         mTabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
             override fun onTabSelected(tab: TabLayout.Tab) {
-                mAdapter.setSelected(tab.position)
+//                mAdapter.setSelected(tab.position)
                 when (tab.position) {
-/*                    MainActivity.POS_COMPONENTS -> switchPage(ComponentsFragment::class.java)
-                    1 -> switchPage(UtilitiesFragment::class.java)
+//                    MainActivity.POS_COMPONENTS -> switchPage(ComponentsFragment::class.java)
+         /*           0 -> switchPage(UtilitiesFragment::class.java)
                     2 -> switchPage(ExpandsFragment::class.java)
                     else -> {
                     }*/
+                    0->{
+                        tab.setIcon(R.drawable.img_home_tab_mingxi)
+                    }
+                    1->{
+                        tab.setIcon(R.drawable.img_home_tab_count)
+                    }
+                    4->{
+                        tab.setIcon(R.drawable.img_home_tab_reward)
+                    }
+                    5->{
+                        tab.setIcon(R.drawable.img_home_tab_my)
+                    }
                 }
             }
 
-            override fun onTabUnselected(tab: TabLayout.Tab) {}
+            override fun onTabUnselected(tab: TabLayout.Tab) {
+                when (tab.position) {
+                    0->{
+                        tab.setIcon(R.drawable.img_home_tab_mingxi_g)
+                    }
+                    1->{
+                        tab.setIcon(R.drawable.img_home_tab_count_g)
+                    }
+                    4->{
+                        tab.setIcon(R.drawable.img_home_tab_reward_g)
+                    }
+                    5->{
+                        tab.setIcon(R.drawable.img_home_tab_my_g)
+                    }
+                }
+            }
             override fun onTabReselected(tab: TabLayout.Tab) {}
         })
     }
