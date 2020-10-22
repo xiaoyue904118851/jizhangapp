9fe3713de77f90438685b0fc195dcf8b8cf7330a
xiaoyue <904118851@qq.com>
2020-10-22 16:23:15 +0800

    12

--- app/src/main/java/com/tool/jizhang/MainActivity.kt
+++ app/src/main/java/com/tool/jizhang/MainActivity.kt
@@ -1,13 +1,15 @@
 package com.tool.jizhang
 
 import android.os.Bundle
-import android.util.SparseArray
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
 import com.google.android.material.tabs.TabLayout
 import com.tool.jizhang.base.BaseActivity
-import com.tool.jizhang.utils.SettingSPUtils
 import com.xuexiang.xui.utils.WidgetUtils
 import kotlinx.android.synthetic.main.activity_main.*
-import java.util.*
+
 
 class MainActivity : BaseActivity() {
 
@@ -49,6 +51,11 @@ class MainActivity : BaseActivity() {
         my.setIcon(R.drawable.img_home_tab_my_g)
         mTabLayout.addTab(my)
 
+
+        mingxi.setCustomView(getTabView("明细",R.drawable.img_home_tab_mingxi_g));
+        count.setCustomView(getTabView("统计",R.drawable.img_home_tab_count_g));
+        reward.setCustomView(getTabView("福利",R.drawable.img_home_tab_reward_g));
+        my.setCustomView(getTabView("我的",R.drawable.img_home_tab_my_g));
         WidgetUtils.setTabLayoutTextFont(mTabLayout)
 
        /* switchPage(ComponentsFragment::class.java)*/
@@ -56,6 +63,9 @@ class MainActivity : BaseActivity() {
         mTabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
             override fun onTabSelected(tab: TabLayout.Tab) {
 //                mAdapter.setSelected(tab.position)
+                val view = tab.customView
+                val img_title =
+                    view?.findViewById(R.id.imageview) as ImageView
                 when (tab.position) {
 //                    MainActivity.POS_COMPONENTS -> switchPage(ComponentsFragment::class.java)
          /*           0 -> switchPage(UtilitiesFragment::class.java)
@@ -63,33 +73,36 @@ class MainActivity : BaseActivity() {
                     else -> {
                     }*/
                     0->{
-                        tab.setIcon(R.drawable.img_home_tab_mingxi)
+                        img_title.setImageResource(R.drawable.img_home_tab_mingxi)
                     }
                     1->{
-                        tab.setIcon(R.drawable.img_home_tab_count)
+                        img_title.setImageResource(R.drawable.img_home_tab_count)
                     }
                     4->{
-                        tab.setIcon(R.drawable.img_home_tab_reward)
+                        img_title.setImageResource(R.drawable.img_home_tab_reward)
                     }
                     5->{
-                        tab.setIcon(R.drawable.img_home_tab_my)
+                        img_title.setImageResource(R.drawable.img_home_tab_my)
                     }
                 }
             }
 
             override fun onTabUnselected(tab: TabLayout.Tab) {
+                val view = tab.customView
+                val img_title =
+                    view?.findViewById(R.id.imageview) as ImageView
                 when (tab.position) {
                     0->{
-                        tab.setIcon(R.drawable.img_home_tab_mingxi_g)
+                        img_title.setImageResource(R.drawable.img_home_tab_mingxi_g)
                     }
                     1->{
-                        tab.setIcon(R.drawable.img_home_tab_count_g)
+                        img_title.setImageResource(R.drawable.img_home_tab_count_g)
                     }
                     4->{
-                        tab.setIcon(R.drawable.img_home_tab_reward_g)
+                        img_title.setImageResource(R.drawable.img_home_tab_reward_g)
                     }
                     5->{
-                        tab.setIcon(R.drawable.img_home_tab_my_g)
+                        img_title.setImageResource(R.drawable.img_home_tab_my_g)
                     }
                 }
             }
@@ -99,5 +112,14 @@ class MainActivity : BaseActivity() {
     override fun getLayoutId(): Int {
         return R.layout.activity_main
     }
-
+    // Tab自定义view
+    fun getTabView(title: String?, image_src: Int): View? {
+        val v: View =
+            LayoutInflater.from(applicationContext).inflate(R.layout.tab_item_view, null)
+        val textView = v.findViewById(R.id.textview) as TextView
+        textView.text = title
+        val imageView: ImageView = v.findViewById(R.id.imageview) as ImageView
+        imageView.setImageResource(image_src)
+        return v
+    }
 }
